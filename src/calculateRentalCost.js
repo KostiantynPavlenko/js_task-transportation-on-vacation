4e4ec606014abb4d27b0f763891e693a269f10b0
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-03-02 12:56:09 +0200

    Rewrote else-if statement

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,18 @@
  */
 function calculateRentalCost(days) {
   const COST_CAR_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
   const totalSum = days * COST_CAR_PER_DAY;
 
-  if (days >= 7) {
-    return totalSum - 50;
-  } else if (days >= 3) {
-    return totalSum - 20;
+  if (days >= LONG_TERM) {
+    const LONG_TERM_DISCOUNT = 50;
+
+    return totalSum - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    const SHORT_TERM_DISCOUNT = 20;
+
+    return totalSum - SHORT_TERM_DISCOUNT;
   }
 
   return totalSum;
