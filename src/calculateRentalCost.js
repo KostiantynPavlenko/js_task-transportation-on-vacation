79f8ac6e962c5ae4c796cec99058459290067458
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-03-02 12:36:27 +0200

    Solution for calculate rental cost task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const COST_CAR_PER_DAY = 40;
+  const totalSum = days * COST_CAR_PER_DAY;
+
+  if (days >= 7) {
+    return totalSum - 50;
+  } else if (days >= 3) {
+    return totalSum - 20;
+  }
+
+  return totalSum;
 }
 
 module.exports = calculateRentalCost;
